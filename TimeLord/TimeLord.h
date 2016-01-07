34d0d935d54ae2c1fafe29660d5fc9f9742cf8ee
Евгений Елчев <yolchev@gmail.com>
2016-01-08 01:06:32 +0700

    Initial Commit

---
+++ TimeLord/TimeLord.h
@@ -0,0 +1,19 @@
+//
+//  TimeLord.h
+//  TimeLord
+//
+//  Created by Евгений Елчев on 08.01.16.
+//  Copyright © 2016 Jon FIr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimeLord.
+FOUNDATION_EXPORT double TimeLordVersionNumber;
+
+//! Project version string for TimeLord.
+FOUNDATION_EXPORT const unsigned char TimeLordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimeLord/PublicHeader.h>
+
+
